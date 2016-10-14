f5faa7a6c625b5ef3fec9d2237ba191bbb9cda2e
David Pfeiffer <davidjpfeiffer@yahoo.com>
2016-10-14 15:22:44 -0500

    Imrpovements to seed data

--- Services/Persistence/Initializer.cs
+++ Services/Persistence/Initializer.cs
@@ -11,18 +11,45 @@ namespace Services.Persistence
         {
             context.ValidateNotNullParameter(nameof(context));
 
-            List<Account> accounts = new List<Account> {
+            List<Account> accounts = GetDefaultAccounts();
+            List<Notebook> notebooks = new List<Notebook>();
+            List<Note> notes = new List<Note>();
+
+            context.Accounts.AddRange(accounts);
+            context.SaveChanges();
+
+            // accounts = await context.Accounts.ToListAsync();
+            accounts.ForEach(account => notebooks.Add(GetDefaultNotebook(account)));
+            context.Notebooks.AddRange(notebooks);
+            context.SaveChanges();
+
+            // notebooks = await context.Notebooks.ToListAsync();
+            notebooks.ForEach(notebook => notes.Add(GetDefaultNote(notebook)));
+            context.Notes.AddRange(notes);
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+
+        private static List<Account> GetDefaultAccounts()
+        {
+            return new List<Account>
+            {
                 new Account("david@noteapplication.com", "David", "Pfeiffer", "Password123"),
                 new Account("steve@noteapplication.com", "Steve", "Haar", "Password123"),
                 new Account("kevin@noteapplication.com", "Kevin", "Phelps", "Password123"),
                 new Account("hunter@noteapplication.com", "Hunter", "LaTourette", "Password123")
             };
+        }
 
-            context.Accounts.AddRange(accounts);
-            context.Notebooks.Add(new Notebook("Davids note", "this is the content", 1));
-            context.SaveChanges();
+        private static Notebook GetDefaultNotebook(Account account)
+        {
+            return new Notebook(@"Default Notebook", "For all newly created notes", account.Id);
+        }
 
-            base.Seed(context);
+        private static Note GetDefaultNote(Notebook notebook)
+        {
+            return new Note("My First Note", "This is your first note. Now go create your own!", notebook.Id);
         }
     }
 }
